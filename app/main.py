e096f67b7f3a8d36b2e6a52c6cbd9f42ab0391c4
BNaz-OTU <bilal.nazar@ontariotechu.net>
2026-05-14 18:01:09 -0400

    feat: Trying to implement a 'locate executables' feature

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 
 BUILTINS = {"echo", "type", "exit"}
 
@@ -8,22 +8,40 @@ def main():
         sys.stdout.write("$ ")
 
         # Take in user input
-        command = input("")
-        arguements = command.split(" ")
+        command_line = input("")
+        arguements = command_line.split(" ")
+
+        command = arguements[0] # Have the command/first value put in the prompt
 
         # Stop the REPL loop/Shell by entering "exit"
         if (command == "exit"):
             break
         
         # Implement the "echo" command
-        elif (arguements[0] == "echo"):
+        elif (command == "echo"):
             print(" ".join(arguements[1:]))
         
         # Implemnt the "type" command, will help check to see what type something is
-        elif (arguements[0] == "type"):
+        elif (command == "type"):
+            
+            path_var = os.environ.get("PATH")
+            directory_paths = path_var.split(":")
+            # print(directory_paths)
+
+            for dir in directory_paths:
+                print(dir)
+                
+            for dir in directory_paths:
+                if (os.access(dir, os.X_OK)):
+                    print(f"{command} is {dir}")
+                    break
+            
+            # CASE 1: BUILTIN
             if (arguements[1] in BUILTINS):
                 print(f"{arguements[1]} is a shell builtin")
-            
+
+
+            # CASE 3: NOT FOUND
             else:
                 print(f"{arguements[1]}: not found")
 

843c9d74a9bb68c321b63d6da1d6b1b0a9ecdc98
BNaz-OTU <bilal.nazar@ontariotechu.net>
2026-05-14 18:02:13 -0400

    fix: Made some modifications to the script that runs the Shell Project

--- your_program.sh
+++ your_program.sh
@@ -12,7 +12,13 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-SCRIPT_DIR="$(dirname "$0")"
+
+SCRIPT_DIR="$(dirname "$0")" # <- dirname will extract the file directory name
+
+# echo $0 # <- "$0" provides the name of where the file is being run from, if I'm in the main directory it would be ".", a diff directory would be "another/directory/bro"
+
+# echo $SCRIPT_DIR 
+
 PYTHONSAFEPATH=1 PYTHONPATH="$SCRIPT_DIR" exec uv run \
   --project "$SCRIPT_DIR" \
   --quiet \
